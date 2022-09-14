0c21b23cbc2ef8f16660854cf80492226401b79c
AdamLipovy <88287063+AdamLipovy@users.noreply.github.com>
2022-09-14 14:35:27 +0200

    Update saver.php

--- creator/saver.php
+++ creator/saver.php
@@ -5,7 +5,7 @@ foreach ($_GET as $key => $value) {
   $i++;
   echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
 }
-if (!mkdir("../Storage/OC/M/Adam", 0777, true)) {
+if (!mkdir("../Storage/OC/M/Martin", 0777, true)) {
     die('Failed to create directories...');
 }
 
